29b4873e104493863a75cf208c0bc1c05f2b1b2c
nezsnt <santiagoibanez2580@gmail.com>
2026-05-09 20:52:23 -0500

    "Login a medias y viewmodel"

--- app/src/main/java/com/example/taller3/navigation/navigation.kt
+++ app/src/main/java/com/example/taller3/navigation/navigation.kt
@@ -1,2 +1,31 @@
 package com.example.taller3.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.taller3.screens.login
+
+enum class AppScreens{
+    authentication,
+    register,
+    home
+
+}
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = AppScreens.register.name){
+        composable(route = AppScreens.authentication.name){
+            login(navController)
+        }
+        composable(route = AppScreens.register.name){
+            //AppScreens.register(navController)
+        }
+        composable(route = AppScreens.home.name){
+            //AppScreens.home()
+        }
+    }
+}
