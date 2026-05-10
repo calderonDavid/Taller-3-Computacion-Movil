61ed99dfd411968ce14c7609b81165aa8dbccb74
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-09 19:35:15 -0500

    Creacion modelo usuario

--- app/src/main/java/com/example/taller3/model/data.kt
+++ app/src/main/java/com/example/taller3/model/data.kt
@@ -1,2 +1,13 @@
 package com.example.taller3.model
 
+data class User(
+    val uid: String = "",
+    val nombre: String = "",
+    val apellido: String = "",
+    val email: String = "",
+    val idNumber: String = "",
+    val profilePicUrl: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val disponible: Boolean = false
+)
