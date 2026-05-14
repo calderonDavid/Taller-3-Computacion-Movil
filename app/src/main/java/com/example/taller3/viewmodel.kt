cb4a6f003053ef86fda1634d45366d9b9cc345a0
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-14 10:50:25 -0500

    Cambio variable isAVailable a available

--- app/src/main/java/com/example/taller3/viewmodel.kt
+++ app/src/main/java/com/example/taller3/viewmodel.kt
@@ -119,7 +119,7 @@ class AuthViewModel: ViewModel(){
         val finalUser = user.copy(
             uid = uid,
             profilePictureUrl = imageUrl,
-            isAvailable = false
+            available = false
         )
         val myRef = database.getReference("users/$uid")
         myRef.setValue(finalUser)
